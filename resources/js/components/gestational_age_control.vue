05fe9017d3147be986fde37df30be8fd25c1c1cc
MHM-IT-2 <mhm-it@mobile-hilfe-madagaskar.de>
2021-08-19 10:44:24 +0300

    Make gestational age control null by default

--- resources/js/components/gestational_age_control.vue
+++ resources/js/components/gestational_age_control.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="form-check-inline">
     <select v-model="gestationalAge.week" :disabled="readonly">
-      <option v-for="i in 40" :value="i">{{ i }}</option>
+      <option v-if="isNull" value="" selected></option>
+      <option v-for="i in 40" :value="i" :key="i">{{ i }}</option>
     </select>
     <strong class="m-auto">+</strong>
     <select v-model="gestationalAge.day" :disabled="readonly">
-      <option v-for="i in 7" :value="i - 1">{{ i - 1 }}</option>
+      <option v-if="isNull" value="" selected></option>
+      <option v-for="i in 7" :value="i - 1" :key="i">{{ i - 1 }}</option>
     </select>
   </div>
 </template>
@@ -15,27 +17,46 @@ export default {
   name: "gestationalAge",
   computed: {
     computedValue() {
+      if (this.isNull)
+        return null;
+
       return this.gestationalAge.week + "+" + this.gestationalAge.day;
     }
   },
   watch: {
     gestationalAge: {
       handler: function () {
+        if (this.gestationalAge.week && this.gestationalAge.day)
+          this.isNull = false;
+
         this.$emit("input", this.computedValue);
       },
       deep: true,
     },
     value() {
+      if (!this.value) {
+        this.isNull = true;
+
+        this.gestationalAge.week = "";
+        this.gestationalAge.day = "";
+      }
+      else {
+        this.isNull = false;
+
         const parts = this.value.split('+');
-        this.gestationalAge.week = parts[0];
-        this.gestationalAge.day = parts[1];
+        this.gestationalAge = {
+          week: parts[0],
+          day: parts[1]
+        };
+      }
     }
   },
   data: function () {
     return {
+      isNull: this.value ? false : true,
       gestationalAge: {
-        week: this.value ? this.value.split('+')[0] : 1,
-        day: this.value ? this.value.split('+')[1] : 0
+        week: this.value ? this.value.split('+')[0] : "",
+        day: this.value ? this.value.split('+')[1] : ""
       },
     };
   },
